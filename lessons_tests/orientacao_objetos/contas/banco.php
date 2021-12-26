fc8272f90e0dfcae295a61dff083b3d6972fc748
rfldesanti <rafaelsr2018@gmail.com>
2021-12-26 07:41:23 -0300

    Metodos e atributos estaticos de classe; getters e setters; __construct();

--- lessons_tests/orientacao_objetos/contas/banco.php
+++ lessons_tests/orientacao_objetos/contas/banco.php
@@ -2,7 +2,7 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta("123.456.780-10", "SOLSTÍCIO");
 var_dump($primeiraConta);
 echo PHP_EOL;
 $primeiraConta->depositar(300);
@@ -10,3 +10,12 @@ var_dump($primeiraConta);
 echo PHP_EOL;
 $primeiraConta->sacar(500);
 var_dump($primeiraConta);
+echo PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+$segundaConta = new Conta("456.789.10-11", "Alfredo");
+//para exibir o atributo estático da classe:
+echo Conta::recuperaNumeroDeContas() . PHP_EOL;
+$terceiraConta = new Conta("898.787.115-32", "EQUADOR");
+echo Conta::recuperaNumeroDeContas() . PHP_EOL;
--- lessons_tests/orientacao_objetos/contas/src/Conta.php
+++ lessons_tests/orientacao_objetos/contas/src/Conta.php
@@ -3,7 +3,30 @@
 class Conta {
     private $cpfTitular;
     private $nomeTitular;
-    private $saldo = 0;
+    private $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(string $cpfTitular, string $nomeTitular) {
+        
+        $this->cpfTitular = $cpfTitular;
+        //a validação não deve ser feita no construtor
+        //para isso, é criado um MÉTODO
+        /*if (strlen($nomeTitular) < 5) {
+            echo "Quantidade de caracteres para nome precisa ser maior";
+            exit();
+        }*/
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+        //"::" (dois-pontos duas vezes) é utilizado para
+        //acessar os atributos estáticos (atributos da classe)
+        //e a variável precisa ser acessada corretamente (com o $);
+        //uma das formas é o próprio nome da classe:
+        //Conta::$numeroDeContas++;
+        //outra é atraves de 'self':
+        self::$numeroDeContas++;
+
+    }
 
     public function sacar(float $valorASacar): void {
 
@@ -37,5 +60,50 @@ class Conta {
         $this->sacar($valorATransferir);
         $contaDestino->depositar($valorATransferir);
         
+    }
+
+    public function recuperaSaldo(): float {
+        
+        return $this->saldo;
+
+    }
+
+    public function recuperaCpfTitular(): string {
+
+        return $this->cpfTitular;
+
+    }
+
+    public function recuperaNomeTitular(): string {
+
+        return $this->nomeTitular;
+
+    }
+
+    //o método construtor já está definindo CPF e nome na criação do objeto
+    //esses métodos abaixo se tornaram obsoletos
+    /*public function defineCpfTitular(string $cpfDoTitular): void {
+
+        $this->cpfTitular = $cpfDoTitular;
+
+    }
+
+    public function defineNomeTitular(string $nomeDoTitular): void {
+
+        $this->nomeTitular = $nomeDoTitular;
+
+    } */
+
+    private function validaNomeTitular(string $nomeTitular) {
+        if(strlen($nomeTitular) < 5) {
+            echo "Quantidade de caracteres para nome precisa ser maior";
+            exit();
+        }
+    }
+
+    public static function recuperaNumeroDeContas(): int {
+        
+        return self::$numeroDeContas;
+
     }
 }
