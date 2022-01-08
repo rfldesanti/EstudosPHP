516b632c1f149e3153286b5755cb96731762d6ce
rfldesanti <rafaelsr2018@gmail.com>
2022-01-08 17:55:14 -0300

    new 'pilha' file | regex filter on class CPF | dealing with exceptions

--- lessons_tests/orientacao_objetos/contas/src/Modelo/CPF.php
+++ lessons_tests/orientacao_objetos/contas/src/Modelo/CPF.php
@@ -6,10 +6,19 @@ final class CPF {
 
     private $cpf;
 
-    public function __construct(string $cpf) {
+    public function __construct(string $cpf)
+    {
+        $cpf = filter_var($cpf, FILTER_VALIDATE_REGEXP, [
+            'options' => [
+                'regexp' => '/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/'
+            ]
+        ]);
 
+        if ($cpf == false) {
+            echo "CPF INVALIDO";
+            exit();
+        }
         $this->cpf = $cpf;
-
     }
 
     public function recuperaCPF(): string {
